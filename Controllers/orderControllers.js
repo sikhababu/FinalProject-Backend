19b93928dd2993523734f85403c16e0dafb4f918
Sikha Babu <sikhababu27@gmail.com>
2025-06-08 19:21:01 +0530

    final commit

--- Controllers/orderControllers.js
+++ Controllers/orderControllers.js
@@ -51,13 +51,14 @@ const createOrder = async (req, res) => {
 
 const listUserOrders = async (req, res) => {
   try {
-    const userId = req.user._id;  
+    const userId = req.user.id;  
 
-    const orders = await OrderModel.find({ userId }).populate("products.productId");
+    const orders = await OrderModel.find({ user : userId }).populate("products.productId");
+    console.log("orders", orders)
     if (!orders || orders.length === 0) {
       return res.status(404).json({ message: "No orders found" });
     }
-
+console.log("orders", orders)
     res.status(200).json(orders);
   } catch (error) {
     console.log(error);
